3ca07ae00b31dbb89b5e3f67c38fd44d9624dc5d
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-12-19 00:56:14 +0530

    make some more change

--- dirwin_os/bin/comp.sh
+++ dirwin_os/bin/comp.sh
@@ -2,92 +2,64 @@
 
 set -e
 
-# Default file path if no argument is provided
-if [[ $# -eq 0 ]]; then
-	if [[ -f "main.c" ]]; then
-		file="main.c"
-	elif [[ -f "main.cpp" ]]; then
-		file="main.cpp"
-	elif [[ -f "main.py" ]]; then
-		file="main.py"
-	elif [[ -f "main.rs" ]]; then
-		file="main.rs"
-	else
-		echo "$0 : missing operand"
-		exit 1
-	fi
-else
-	file="$1"
-fi
+file="$1"
 
 file=`realpath $file`
 
 path=$(dirname "$file")
 filename=$(basename "$file")
 
+run_command=""
+
 # Checking file type and setting up the run command
 case "$file" in
 	*.c)
-		run_command="$CC -std=c2x "
+		run_command+="$CC -std=c17 "
 		;;
 	*.cpp)
-		run_command="$CXX -std=c++2b "
+		run_command+="$CXX -std=c++20 "
 		;;
 	*.rs)
-		cargo run -q
-		exit $?
+		run_command+="cargo run -q "
 		;;
 	*.pl)
-		perl "$file"
-		exit $?
+		run_command+="perl $file "
 		;;
 	[Mm]akefile)
-		make -C "$path" -f "$file"
-		exit $?
+		run_command+="make -c $path -f $file "
 		;;
 	*.html)
-		open "$file"
-		exit $?
+		run_command+="open $file "
 		;;
 	*.d2)
-		d2 "$file"
-		exit $?
+		run_command+="d2 $file "
 		;;
 	*.cal)
-		bc -lf "$file"
-		exit $?
+		run_command+="bc -lf $file "
 		;;
 	*.py)
-		python3 "$file"
-		exit $?
+		run_command+="python3 $file "
 		;;
 	*.go)
-		go run "$file"
-		exit $?
+		run_command+="go run $file "
 		;;
 	*.zig)
-		zig run "$file"
-		exit $?
+		run_command+="zig run $file "
 		;;
 	*.sql)
-		sqlite3 < "$file"
-		exit $?
+		run_command+="sqlite3 < $file "
 		;;
 	*.cs)
-		dotnet run
-		exit $?
+		run_command+="dotnet run "
 		;;
 	*.java)
-		java "$file"
-		exit $?
+		run_command+="javac $file "
 		;;
 	*.js)
-		node "$file"
-		exit $?
+		run_command+="node $file "
 		;;
 	*.sh)
-		bash "$file"
-		exit $?
+		run_command+="bash $file "
 		;;
 	*)
 		shebang=$(head -n 1 "$file" | cut -c 3-)
@@ -103,15 +75,17 @@ esac
 
 RUN=0
 
+
+if [[ ! $run_command =~ ^/usr/bin/clang ]]; then
+	echo $run_command >&2; printf '%*s\n' `tput cols` '' | tr ' ' '─' >&2
+	eval $run_command
+	exit $?
+fi
+
 if [[ "$2" == '-' ]]; then
 	RUN=1
 	TMPDIR="$path/"
 	shift 1
-fi
-
-
-if [[ "$path" == "$HOME/Developer/leedcode"* ]]; then
-	run_command+="$INC_FLAGS $LDLIB "
 fi
 
 run_command+="-pedantic -Wall -arch arm64 $2 $3 $4 $5 $6 $7 $8 $9 '$path/$filename' -o '$TMPDIR${filename//./-}.out'"
