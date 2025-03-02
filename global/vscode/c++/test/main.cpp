d3d46873ee0c717cac16dcaba418ef7a6fad800f
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-03-02 15:47:24 +0530

    chagne

--- global/vscode/c++/test/main.cpp
+++ global/vscode/c++/test/main.cpp
@@ -3,14 +3,10 @@
 // #include "../src/something.hpp"
 
 TEST(FactorialTest, FactorialOfPositiveNos) {
-  /*
   ASSERT_EQ(1, factorial(1));
-  ASSERT_EQ(24, factorial(4));
-  ASSERT_EQ(120, factorial(5));
- */
 }
 
-int main(int argc, char *argv[]) {
-  ;
-  return 0;
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
