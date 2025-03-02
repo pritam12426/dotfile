ca71fd033ddf9d4389f29eed172b55f77c974e8b
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-03-02 13:18:26 +0530

    chane

--- global/vscode/c++/test/src/functions.cpp
+++
@@ -1,5 +0,0 @@
-#include "functions.hpp"
-
-int Fun::add(int32_t x, int32_t y) {
-	return x * y;
-}
--- global/vscode/c++/test/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "functions.hpp"
-
-int main(int argc, char *argv[]) {
-	// std::cout << PROJECT_NAME << '\n';
-	// int a = Fun::add(2, 2);
-	// std::cout << a << '\n';
-	return 0;
-}
