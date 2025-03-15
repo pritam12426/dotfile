c5334c20cda8e97fd1f7d6f8bc67d506854060a4
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-03-15 11:36:34 +0530

    change

--- dirwin_os/.config/lf/pv.sh
+++ dirwin_os/.config/lf/pv.sh
@@ -2,13 +2,25 @@
 
 file_type=$(file --mime-type -b "$1")
 
-# *.tar*) tar tf "$1";;
-# *.zip) unzip -l "$1";;
-# *.rar) unrar l "$1";;
-# *.7z) 7z l "$1";;
+case "$1" in
+	*.tar*)
+		tar tf "$1"
+	;;
+	*.zip)
+		unzip -l "$1"
+	;;
+	*.rar)
+		unrar l "$1"
+	;;
+	*.7z)
+		7z l "$1"
+	;;
 
-if [[ $file_type == text/* ]]; then
-	bat --paging=never --style=numbers --wrap=never --terminal-width $(($2-5)) -f "$1" || true
-else
-	exiftool -FileSize -FilePermissions -FileType -CreateDate -ModifyDate -TimeScale -Duration -TrackCreateDate -TrackModifyDate -MediaLanguageCode -HandlerType -ImageSize "$1"
-fi
+	*)
+		if [ "${file_type%%/*}" = "text" ]; then
+			bat --paging=never --style=numbers --wrap=never -f "$1" || true
+	else
+			exiftool -FileSize -FilePermissions -FileType -CreateDate -ModifyDate -TimeScale -Duration -TrackCreateDate -TrackModifyDate -MediaLanguageCode -HandlerType -ImageSize "$1"
+		fi
+		;;
+esac
