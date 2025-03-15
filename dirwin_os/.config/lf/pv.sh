0a90cf40f0fba0aabed97e1e82c6fca533690f59
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-03-15 11:22:19 +0530

    chanet

--- dirwin_os/.config/lf/pv.sh
+++ dirwin_os/.config/lf/pv.sh
@@ -2,6 +2,11 @@
 
 file_type=$(file --mime-type -b "$1")
 
+# *.tar*) tar tf "$1";;
+# *.zip) unzip -l "$1";;
+# *.rar) unrar l "$1";;
+# *.7z) 7z l "$1";;
+
 if [[ $file_type == text/* ]]; then
 	bat --paging=never --style=numbers --wrap=never --terminal-width $(($2-5)) -f "$1" || true
 else
