ef2e48069f4d35636a0ad9264cd965aceb719dd0
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-03-31 15:11:59 +0530

    some thing is chagne

---
+++ global/vscode/c/test/main.c
@@ -0,0 +1,18 @@
+#include <unity/unity.h>
+
+#include "../src/functions.h"
+
+void setUp(void) {}     // Required by Unity
+void tearDown(void) {}  // Required by Unity
+
+void test_add(void) {
+	TEST_ASSERT_EQUAL(5, add(2, 3));
+	TEST_ASSERT_EQUAL(-1, add(2, -3));
+	TEST_ASSERT_EQUAL(0, add(0, 0));
+}
+
+int main(void) {
+	UNITY_BEGIN();
+	RUN_TEST(test_add);
+	return UNITY_END();
+}
