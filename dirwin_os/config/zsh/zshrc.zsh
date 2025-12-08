0e14a80dc2d0fb641a3535efd3a81d15a73f0a7d
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-12-08 21:20:28 +0530

    I have commmited a file

--- dirwin_os/config/zsh/zshrc.zsh
+++ dirwin_os/config/zsh/zshrc.zsh
@@ -1,5 +1,6 @@
 export LIBS_DIR="/usr/local/big_library"
-export EDITOR="$HOME/.local/bin/nvim"
+# export EDITOR="$HOME/.local/bin/nvim"
+export EDITOR="$HOME/.local/bin/hx"
 export DOT_FILE="$HOME/Developer/git_repository/my_dotfile/dirwin_os/"
 
 export CXX="/usr/bin/clang++"
