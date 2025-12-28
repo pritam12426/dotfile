e6f7d1f83f9c0af2b257ace8d4101b829c3b04c5
Pritam <84720825+pritam12426@users.noreply.github.com>
2025-12-28 11:38:43 +0530

    I have make some chagne

--- dirwin_os/scripts/comp.sh
+++ dirwin_os/scripts/comp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
@@ -15,20 +15,20 @@ run_command=""
 # Checking file type and setting up the run command
 case "$file" in
 *.c)
-	# run_command+="$CC -std=c17 "$file" -fsanitize=address "
-	run_command+="$CC -std=c17 "$file" "
+	# run_command+="$CC -std=c17 $file -fsanitize=address "
+	run_command+="$CC -std=c17 $file "
 	;;
 *.cpp)
-	run_command+="$CXX -std=c++20 "$file" "
+	run_command+="$CXX -std=c++20 $file "
 	;;
 *.rs)
 	run_command+="cargo run "
 	;;
 *.pl)
-	run_command+="perl "$file" "
+	run_command+="perl $file "
 	;;
 */[Mm]akefile)
-	run_command+="make -C $path -f "$file" "
+	run_command+="make -C $path -f $file "
 	;;
 */CMakeLists.txt)
 	LIB="${PWD##*/}"
@@ -42,52 +42,52 @@ case "$file" in
 
 	;;
 *.html)
-	run_command+="open "$file" "
+	run_command+="open $file "
 	;;
 *.d2)
 	first_line=$(head -n 1 "$file")
 
 	if [[ $first_line == \#!* ]]; then
-		run_command+="${first_line:2} "$file" "
+		run_command+="${first_line:2} $file "
 	else
-		run_command+="d2 --watch --layout elk --center "$file" "
+		run_command+="d2 --watch --layout elk --center $file "
 	fi
 
 	;;
 *.cal)
-	run_command+="bc -liqf "$file" "
+	run_command+="bc -liqf $file "
 	;;
 *.py)
-	run_command+="python3 "$file" "
+	run_command+="python3 $file "
 	;;
 *.lua)
-	run_command+="lua "$file" "
+	run_command+="lua $file "
 	;;
 *.go)
-	run_command+="go run "$file" "
+	run_command+="go run $file "
 	;;
 *.zig)
-	run_command+="zig run "$file" "
+	run_command+="zig run $file "
 	;;
 *.sql)
-	run_command+="sqlite3 -header -table -bail -nullvalue "-null-" $path/$file_without_extension.sqlite3 < "$file" "
-	# run_command+="sqlite3 -header -table -bail -nullvalue \"\u{F9E2}\" < "$file" "
+	run_command+="sqlite3 -header -table -bail -nullvalue '-null-' $path/$file_without_extension.sqlite3 < $file "
+	# run_command+="sqlite3 -header -table -bail -nullvalue \"\u{F9E2}\" < $file "
 	;;
 *.cs)
 	run_command+="dotnet run "
 	;;
 *.java)
-	run_command+="java "$file" "
+	run_command+="java $file "
 	;;
 *.js)
-	run_command+="node "$file" "
+	run_command+="node $file "
 	;;
 *.ts)
-	run_command+="node "$file" "
+	run_command+="node $file "
 	;;
 *.sh | *.bash | *.zsh)
-	# run_command+="bash -x "$file" "
-	run_command+="bash "$file" "
+	# run_command+="bash -x $file "
+	run_command+="bash $file "
 	;;
 *)
 	$file
@@ -97,8 +97,8 @@ esac
 RUN=0
 
 if [[ ! $run_command =~ ^/usr/bin/clang ]]; then
-	echo $run_command >&2; printf '%*s\n' $(tput cols) '' | tr ' ' '-' >&2
-	eval $run_command
+	echo "$run_command" >&2; printf '%*s\n' "$(tput cols)" '' | tr ' ' '-' >&2
+	eval "$run_command"
 	exit $?
 fi
 
@@ -111,11 +111,12 @@ fi
 run_command+="-pedantic -Wall -arch arm64 $2 $3 $4 $5 $6 $7 $8 $9 -o '$TMPDIR${filename//./-}.out'"
 
 # Execute the run command
-echo $run_command >&2; printf '%*s\n' $(tput cols) '' | tr ' ' '-' >&2
-eval $run_command
+echo "$run_command" >&2; printf '%*s\n' "$(tput cols)" '' | tr ' ' '-' >&2
+eval "$run_command"
+return_code=$!
 
 if [[ $RUN == 1 ]]; then
-	exit $?
+	exit $return_code
 fi
 
 # Run the compiled output if compilation was successful
